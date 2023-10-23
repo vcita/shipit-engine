6e975390b04b12b5f3ed04a98dadaa37785dc787
michael.shemesh <michael.shemesh@vcita.com>
2023-10-23 10:13:00 +0300

    VCITA-34756 : Delete predictive branch after successful merge

--- lib/shipit/predictive_build_commands.rb
+++ lib/shipit/predictive_build_commands.rb
@@ -58,6 +58,10 @@ module Shipit
       git('rev-list', "#{from}..#{to}",'--first-parent', chdir: @chdir)
     end
 
+    def git_delete_branch(branch)
+      git('push', 'origin', '-d', branch, chdir: @chdir, env: env)
+    end
+
     def create_directories
       FileUtils.mkdir_p(@stack.builds_path)
     end
