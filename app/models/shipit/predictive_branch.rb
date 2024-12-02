7fefa344042a8ab69bbee239158c979f0acd1701
Avner Lavi <avner.lavi@vcita.com>
2024-12-02 14:41:33 +0200

    changed predictive branch error message to avoid confucion regarding pipeline restart

--- app/models/shipit/predictive_branch.rb
+++ app/models/shipit/predictive_branch.rb
@@ -242,7 +242,7 @@ module Shipit
       predictive_build.predictive_branches.each do |p_build_branch|
         failed_branches << p_build_branch if p_build_branch.failed?
       end
-      return "Something went wrong, we had to start over." if failed_branches.empty?
+      return "Something went wrong, please start over." if failed_branches.empty?
       res = []
       res << "We had to start over, we failed to process your request due to CI failures of the following projects: "
       failed_branches.each do |fb|
